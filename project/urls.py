a62ce6e8b1e881321b66d4c8d80f72666ee813d4
liliana-karpenko <lilianakarpenko12@gmail.com>
2022-05-01 21:51:47 +0300

    refactor html

--- project/urls.py
+++ project/urls.py
@@ -19,7 +19,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', TemplateView.as_view(template_name='Home.html'), name='Home'),
-    # path('users/', include('users.urls'))
     path('', include('users.urls'))
 ]
